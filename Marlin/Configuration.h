3872042f09deb9f16ef33e1986fbe17dd353bafb
otioss <otioss@protonmail.com>
2021-03-14 19:11:09 +0700

    improve steps

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -800,7 +800,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 420, 400 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 81.2, 80.60, 403.80, 400 }
 
 /**
  * Default Max Feed Rate (mm/s)
